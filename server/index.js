6c79f001eac9e62a39c88bcd9d6a9eb9f8ba2f99
HatemKhabir <hatemkhabir.cse@gmail.com>
2024-01-25 11:02:57 +0100

    pingmeetpong base functionalities

--- server/index.js
+++ server/index.js
@@ -31,65 +31,10 @@ app.delete("/test/user",async (req,res)=>{
   }
 })
 
-// app.post("/api/login", (req, res) => {
-//   const { username, password } = req.body
-//   Player.findOne({ username: username }).then((user) => {
-//     if (user) {
-//       if (user.password === password) {
-//         // Set a dummy authentication token
-//         const authToken = "dummy_auth_token"
-
-//         // Set the authorization header and send the token in the response
-//         res.setHeader("authorization", authToken)
-//         res.json("success")
-//       } else res.json("password incorrect")
-//     } else {
-//       res.json("username doesn't exist")
-//     }
-//   })
-// })
-
-//? First approach
-// app.post("/api/signup", (req, res) => {
-//   Player.create(req.body)
-//     .then((result) => res.json(result))
-//     .catch((err) => res.json(err))
-// })
-
-//? second approach -->most probably wrong coz it doesn't link to mongoDB
-// app.post("/api/signup", async (req, res) => {
-//   try {
-//     const response = await axios.post(
-//       "http://localhost:8080/api/signup",
-//       JSON.stringify(req.body),
-//       {
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//       }
-//     )
-//     res.json(response.data)
-//   } catch (error) {
-//     res.json(error)
-//   }
-// })
-
-//? third approach
-
-// app.post("/api/signup", async (req, res) => {
-//   console.log(req.body) // Log the request body to check if data is received correctly
-//   try {
-//     const newUser = await User.create(req.body) // Create a new user in the database
-//     res.json(newUser) // Send the newly created user as the response
-//   } catch (error) {
-//     res.json(error)
-//   }
-// })
-
 const startServer = async () => {
   try {
-    connectDB(process.env.ATLAS_URI)
     app.listen(port, () => console.log(`server has started on port ${port}`))
+    connectDB(process.env.ATLAS_URI)
   } catch (e) {
     console.log(e)
   }
